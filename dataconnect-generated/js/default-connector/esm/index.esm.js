bdc2042d120768036bd7c77f2ee085615f8a6cec
leo1967as <narawhich2547@gmail.com>
2025-04-26 12:55:31 +0700

    Current Work

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'tissueboxwip',
+  location: 'us-central1'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,7 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'tissueboxwip',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
